d408c40fbb7726cc5f91b3e4740a8ff02860d435
Zamor <zamor2@wp.pl>
2015-11-05 14:23:00 +0100

    dodane suma modulo

--- bit.py
+++ bit.py
@@ -4,7 +4,9 @@ __author__ = 'Przemysław Zamorski'
 __description__ = 'Pierwsze zadanie suma parzystości bodajże '
 
 file = ''
-
+mod_file=''
+##BIT PARZYSTOSCI##
+print('BIT PARZYSTOSCI')
 # wczytanie pliku do wersji binarnej i zliczenie bitów
 with open('test.txt', 'rb') as f1:
     while True:
@@ -34,11 +36,12 @@ def moj_random(plik):
     # for petla in range(0,2): #petla dla zmiany 2 znakow
     for petla in range(random.randint(0, 20)): #petla dla randowowej liczby znakow
         pos = random.randint(0, len(plik) - 1)
-        print('pos-',pos)
         if plik[pos] == '1':
             plik = plik[:pos] + "0" + plik[pos + 1:]
-        else:
+        elif plik[pos] == '0':
             plik = plik[:pos] + "1" + plik[pos + 1:]
+        else:
+            pass
     return plik
 
 #zliczenie 1 parzystosci
@@ -57,9 +60,49 @@ random_file.close()
 
 
 if parzystosc(file[:len(file)-1]) == file[len(file)-1:]:
-    print('jest ok')
+    print('Bit parzystosci - pliki sie zagdzaja')
+    print('')
 else:
-    print('Nope - nie sa takie same')
+    print('Bit parzystosci - pliki się nie zagadzaja')
+    print('')
+
+##SUMA MODULO##
+print('SUMA MODULO')
+
+#funkcja zliczajaca jedynki
+def zlicz(plik):
+    counter = 0
+    for x in plik:
+            if x == '1':
+                counter += 1
+    return counter
+
+
+
+#wczytywanie binarne pliku
+with open('test.txt', 'rb') as f1:
+    while True:
+        bytes = f1.read(1024)
+        if bytes:
+            for byte in bytes:
+                mod_file = ''.join((mod_file, bin(byte)))
+        else:
+            break
+
+print('plik binarny wczytany do testu', mod_file)
+first_mod=zlicz(mod_file)%12
+
+mod_random=moj_random(mod_file)
+print('Plik w sumie modulo po zakloceniach' , mod_random)
+second_mod=zlicz(mod_random)%12
+
+if first_mod==second_mod:
+    print('Suma modulo -  pliki zgadzaja sie')
+    print('')
+
+else:
+    print('Suma modulo - pliki sie nie zgadzaja')
+    print('')
 
 
 
