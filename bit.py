17cf393153bd280e2308a9b785c39dee19c14b43
Zamor <zamor2@wp.pl>
2015-11-05 12:20:23 +0100

    dodanie funkcji random następnie zapis do pliku

--- bit.py
+++ bit.py
@@ -1,3 +1,5 @@
+import random
+
 __author__ = 'Przemysław Zamorski'
 __description__ = 'Pierwsze zadanie suma parzystości bodajże '
 
@@ -17,10 +19,10 @@ with open('test.txt', 'rb') as f1:
 
 for x in file:
     if x== '1':
-        print(x)
         counter+=1
 
 print('plik bez parzystosci',file)
+
 
 #sprawdzenie czy jest parzyste i dodanie oraz zapisanie w pliku
 if (counter%2 == 0):
@@ -34,18 +36,28 @@ second_file.write(file)
 second_file.close()
 
 
+#random funkcja zamienic na replece i zabezpieczyc sie przed b
+def moj_random(plik):
+    pos=random.randint(0,len(plik)-1)
+    if plik[pos] == '1':
+      plik=plik[:pos] +"0"+plik[pos+1:]
+    else:
+      plik=plik[:pos] +"1"+plik[pos+1:]
+    return plik
+
+#wykonanie randoma
+file=moj_random(file)
+print('plik  po   losowaniu', file)
+random_file=open('random.txt', 'w')
+random_file.write(file)
+random_file.close()
 
 
 
 
-# #def get_random_noise():
-#     # if random.randint(0, 100)==1:
-#         pos = random.randint(0, 8)
-#         return pos
-#     # return 0
-# tu masz losowanie pozycji a w komencie masz losowanie prawdopodobieństwa
-# #
-# # print(binary)
+
+
+
 
 
 
@@ -57,3 +69,12 @@ second_file.close()
 # 3 zsumuj 1 i zapisz jak jest parzysta 1 a jak nie 0
 # 4 na tej liście zrób random change
 # 5 wynik po randomie sumuje parzystość i sprawdzasz
+
+# #def get_random_noise():
+#     # if random.randint(0, 100)==1:
+#         pos = random.randint(0, 8)
+#         return pos
+#     # return 0
+# tu masz losowanie pozycji a w komencie masz losowanie prawdopodobieństwa
+# #
+# # print(binary)
